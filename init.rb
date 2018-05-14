30bfa9107b7c137ed20277b52a349d252061f6c6
Daniel Staněk <daniel.stanek@techlib.cz>
2018-05-14 15:08:03 +0200

    Fix README.rdoc and init.rb with correct information.
    Signed-off-by: Daniel Staněk <daniel.stanek@techlib.cz>

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :link do
   require_dependency 'FileLinkHook'
   name 'Link plugin'
   author 'Daniel Staněk'
-  description 'This plugin fills custom field in new issue with dynamically generated link'
+  description 'This plugin displays custom field in issue with dynamically generated link'
   version '0.0.1'
   url 'http://github.com/techlib/link_plugin'
   author_url 'http://www.techlib.cz'
